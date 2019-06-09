6f7bd241c2616e961aceed1328dd942f5fe74f7d
Valmir <vcinquini@gmail.com>
2019-06-09 10:31:07 -0300

    Initial commit

---
+++ ApiGateway/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Ocelot.Middleware;
+using Ocelot.DependencyInjection;
+
+namespace ApiGateway
+{
+	public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+		public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+		=>	WebHost.CreateDefaultBuilder(args)
+				.ConfigureAppConfiguration((host, config) =>
+				{
+					config.AddJsonFile("ocelot.json");
+				})
+				.UseStartup<Startup>();
+    }
+}
---
+++ CatalogApi/Controllers/CurrenciesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogApi.Controllers
+{
+    [Route("api/currencies")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+		// GET api/currencies
+		[HttpGet, Route("")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "usd", "brl", "jpy", "eur" };
+        }
+
+    }
+}
